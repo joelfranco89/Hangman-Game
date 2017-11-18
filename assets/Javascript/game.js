38c869b6cc41b3a935db06a71c1e15b787a9c894
Joel Franco <joel.franco0789@gmail.com>
2017-11-17 20:32:30 -0600

    updates

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -1,77 +1,145 @@
 var wins = 0
-var words = {
-	word1: ["a", "p", "p", "l", "e"],
-	word2: ["g", "o", "o", "g", "l", "e"],
-	word3: ["m", "i", "c", "r", "o", "s", "o", "f", "t"]
-}
-var wordsIndex = 0
-var wordsArray = [words.word1, words.word2, words.word3]
 var wordCount = 1
 var numberOfGuessesRemaining = 12
-function hideAppleWord(){
-	document.getElementById("a1").style.display = "none"
-	document.getElementById("p1").style.display = "none"
-	document.getElementById("p1a").style.display = "none"
-	document.getElementById("l1").style.display = "none"
-	document.getElementById("e1").style.display = "none"
+var lettersAlreadyGuessed = 0
+function reset() {
+	numberOfGuessesRemaining = 12
+	lettersAlreadyGuessed = 0
 }
-function hideGoogleWord(){
-	document.getElementById("g2").style.display = "none"
-	document.getElementById("o2").style.display = "none"
-	document.getElementById("o2a").style.display = "none"
-	document.getElementById("g2a").style.display = "none"
-	document.getElementById("l2").style.display = "none"
-	document.getElementById("e2").style.display = "none"
+function hideApple(){
+	document.getElementById("appleDisplay").style.display = "none";
 }
-function hideMicrosoftWord(){
-	document.getElementById("m3").style.display = "none"
-	document.getElementById("i3").style.display = "none"
-	document.getElementById("c3").style.display = "none"
-	document.getElementById("r3").style.display = "none"
-	document.getElementById("o3").style.display = "none"
-	document.getElementById("s3").style.display = "none"
-	document.getElementById("o3a").style.display = "none"
-	document.getElementById("f3").style.display = "none"
-	document.getElementById("t3").style.display = "none"
+function hideGoogle(){
+	document.getElementById("googleDisplay").style.display = "none";
 }
-function showAppleWord(){
-	document.getElementById("a1").style.display = "none"
-	document.getElementById("p1").style.display = "none"
-	document.getElementById("p1a").style.display = "none"
-	document.getElementById("l1").style.display = "none"
-	document.getElementById("e1").style.display = "none"
-}
-function showGoogleWord(){
-	document.getElementById("g2").style.display = "none"
-	document.getElementById("o2").style.display = "none"
-	document.getElementById("o2a").style.display = "none"
-	document.getElementById("g2a").style.display = "none"
-	document.getElementById("l2").style.display = "none"
-	document.getElementById("e2").style.display = "none"
-}
-function showMicrosoftWord(){
-	document.getElementById("m3").style.display = "none"
-	document.getElementById("i3").style.display = "none"
-	document.getElementById("c3").style.display = "none"
-	document.getElementById("r3").style.display = "none"
-	document.getElementById("o3").style.display = "none"
-	document.getElementById("s3").style.display = "none"
-	document.getElementById("o3a").style.display = "none"
-	document.getElementById("f3").style.display = "none"
-	document.getElementById("t3").style.display = "none"
-}
-
-	if (wordCount == 1){
-		hideGoogleWord()
-		hideMicrosoftWord()
+if (wordCount == 1){
 		document.onkeyup = function(event){
-		switch (event.key){
-			case "a":
-			document.getElementById("a1").style.display = "block"
+			switch(event.key){
+				case "a":
+				lettersAlreadyGuessed += 1
+				document.getElementById("a1").innerHTML = "a"
+				break;
+				case "p":
+				document.getElementById("p1").innerHTML = "p"
+				document.getElementById("p1a").innerHTML = "p"
+				lettersAlreadyGuessed += 1
+				break;
+				case "l":
+				document.getElementById("l1").innerHTML = "l"
+				lettersAlreadyGuessed += 1
+				break;
+				case "e":
+				document.getElementById("e1").innerHTML = "e"
+				lettersAlreadyGuessed += 1
+				break;
+				default:
+				numberOfGuessesRemaining -= 1
+			}
+			if (lettersAlreadyGuessed == 4){	
+				wordCount += 1
+				wins += 1
+				document.getElementById("wins").innerHTML = wins
+				reset()
+				hideApple()
+				alert("You win!")
+			}
+			if (numberOfGuessesRemaining == 0){
+			alert("Game over! Try again!")
+			reset()
+			}
+			console.log(wordCount)
+
+		document.getElementById("numberOfGuesses").innerHTML = numberOfGuessesRemaining
+		document.getElementById("lettersGuessed").innerHTML = lettersAlreadyGuessed
 		}
-	}
-		
-	
-
-
 }
+if (wordCount == 2){
+		document.onkeyup = function(event){
+			switch(event.key){
+				case "g":
+				lettersAlreadyGuessed += 1
+				document.getElementById("g2").innerHTML = "a"
+				document.getElementById("g2a").innerHTML = "a"
+				break;
+				case "o":
+				document.getElementById("o2").innerHTML = "p"
+				document.getElementById("o2a").innerHTML = "p"
+				lettersAlreadyGuessed += 1
+				break;
+				case "e":
+				document.getElementById("l2").innerHTML = "e"
+				lettersAlreadyGuessed += 1
+				break;
+				case "e":
+				document.getElementById("e2")
+				lettersAlreadyGuessed += 1
+				default:
+				numberOfGuessesRemaining -= 1
+			}
+			if (lettersAlreadyGuessed === 4){
+				wordCount += 1
+				wins += 1
+				document.getElementById("wins").innerHTML = wins
+				reset()
+				hideApple()
+				alert("You win!")
+			}
+			if (numberOfGuessesRemaining == 0){
+			alert("Game over! Try again!")
+			reset()
+			}
+
+		document.getElementById("numberOfGuesses").innerHTML = numberOfGuessesRemaining
+		document.getElementById("lettersGuessed").innerHTML = lettersAlreadyGuessed
+		}
+}
+if (wordCount == 3){
+		document.onkeyup = function(event){
+			switch(event.key){
+				case "m":
+				lettersAlreadyGuessed += 1
+				document.getElementById("m3").innerHTML = "a"
+				break;
+				case "i":
+				document.getElementById("i3").innerHTML = "p"
+				lettersAlreadyGuessed += 1
+				break;
+				case "c":
+				document.getElementById("c3").innerHTML = "e"
+				lettersAlreadyGuessed += 1
+				break;
+				case "r":
+				document.getElementById("r3").innerHTML = "r"
+				lettersAlreadyGuessed += 1
+				case "o":
+				document.getElementById("o3").innerHTML = "o"
+				document.getElementById("o3a").innerHTML = "o"
+				lettersAlreadyGuessed += 1
+				case "s":
+				document.getElementById("s3").innerHTML = "s"
+				lettersAlreadyGuessed += 1
+				case "f":
+				document.getElementById("f3").innerHTML = "f"
+				lettersAlreadyGuessed += 1
+				case "t":
+				document.getElementById("t3").innerHTML = "t"
+				lettersAlreadyGuessed += 1
+				default:
+				numberOfGuessesRemaining -= 1
+			}
+			if (lettersAlreadyGuessed === 4){
+				alert("You win!")
+				wordCount += 1
+				reset()
+			}
+			if (numberOfGuessesRemaining == 0){
+			alert("Game over! Try again!")
+			reset()
+			}
+
+		document.getElementById("numberOfGuesses").innerHTML = numberOfGuessesRemaining
+		document.getElementById("lettersGuessed").innerHTML = lettersAlreadyGuessed
+		}
+}
+
+
